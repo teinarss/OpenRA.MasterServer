c555d13f41b5946e9f82e245fa258cd5c177905a
teinarss <einarsson.tomas@gmail.com>
2021-01-30 20:08:23 +0100

    Init

---
+++ OpenRA.MasterServer/Controllers/ServerController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace OpenRA.MasterServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServerController : ControllerBase
+    {
+        private readonly MasterServerContext _context;
+
+        private static List<Server> servers = new List<Server>();
+
+        public ServerController(MasterServerContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Response>> Get()
+        {
+            //var servers = await _context.Servers.ToListAsync();
+
+            return Ok(servers);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Response>> Post([FromBody]Server server)
+        {
+            try
+            {
+                var remoteAddress = HttpContext.Connection.RemoteIpAddress;
+                var address = server.Address.Split(':');
+                //var ou = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.Tcp);
+                var open = PingHost(remoteAddress, Int32.Parse(address[1]));
+                if (!open)
+                    return NotFound();
+
+                servers.Add(server);
+
+                var ser = new Server();
+
+                //await _context.Servers.AddAsync(ser);
+                //await _context.SaveChangesAsync();
+            }
+            catch (SocketException e)
+            {
+                return NotFound();
+            }
+
+
+            return Ok();
+        }
+
+        public static bool PingHost(IPAddress address,  int port)
+        {
+            
+
+            try
+            {
+                // var hosts = Dns.GetHostAddresses(host[0]);
+                //var endpoint = new IPEndPoint(host., int.Parse(host[1]));
+                //var endpoint = new DnsEndPoint(host[0], Int32.Parse(host[1]));
+
+                //var client = new TcpClient(endpoint.AddressFamily) { NoDelay = true };
+                var client = new TcpClient();
+                client.Connect(address, port);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+
+    public class Response
+    {
+
+    }
+}
---
+++ OpenRA.MasterServer/Startup.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace OpenRA.MasterServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<MasterServerContext>(options =>
+                options.UseInMemoryDatabase(Configuration.GetConnectionString("DefaultConnection")));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+
+    public class MasterServerContext : DbContext
+    {
+        public MasterServerContext(DbContextOptions<MasterServerContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Server> Servers { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Server>().ToTable("Servers");
+        }
+    }
+}
